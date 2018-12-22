3b7e655c58c1def4ee7592c533d35b5d521204ac
Ignacio Buioli <ibuioli@gmail.com>
2018-12-22 19:01:06 -0300

    feat: filter of basic dirty signal, selection modes with switch and re-calibrate with button

--- emf-detector.ino
+++ emf-detector.ino
@@ -1,33 +1,70 @@
 #define sample 500
 
-int inPinH = 0;
+byte inPinH = 0;
 float valH;
 
-int pin = 9;
+int dirty[10];
+byte index = 0;
+int maxVal = 0;
+
+//byte pin = 9;
+
+volatile boolean state = false;
 
 void setup() {
   Serial.begin(9600);
+
+  pinMode(2, INPUT);  //To Switch
+  pinMode(3, INPUT_PULLUP); //To Button
 }
 
-void loop(){
+void loop() {
   valH = average(inPinH, sample);
 
-  valH = constrain(valH, 0, 100);
-  valH = map(valH, 0, 100, 0, 255);
+  //valH = constrain(valH, 0, 100);
+  //valH = map(valH, 0, 100, 0, 255);
   //analogWrite(pin, valH);
 
+  if (digitalRead(2) == 1) {  //Default Calibration
+    if (millis() > 2000) {
+      if (index < 10) {
+        dirty[index] = valH;
+        index++;
+      } else {
+        for (byte i = 1; i < 10; i++) {
+          maxVal = max(dirty[i], maxVal);
+        }
+      }
+    }
+  }
+
   delay(1);
-  Serial.println(valH);
+
+  if (digitalRead(2) == 1) {
+    attachInterrupt(digitalPinToInterrupt(3), interrupt, FALLING);
+    Serial.println(jail(valH, maxVal)); //Clear signal
+  } else {
+    Serial.println(valH); //Dirty signal
+  }
 }
 
-float average(int inPin, int sam){
+float average(int inPin, int sam) {
   int arr[sam];
   unsigned long average = 0;
 
-  for(int i = 0; i < sam; i++){
-     arr[i] = analogRead(inPin);
-     average += arr[i];
+  for (int i = 0; i < sam; i++) {
+    arr[i] = analogRead(inPin);
+    average += arr[i];
   }
 
   return average / sam;
+}
+
+float jail(float avg, int dirty) {
+  return max(0, avg - dirty);
+}
+
+void interrupt() {  //Re-calibrate
+  index = 0;
+  maxVal = 0;
 }
